b3647bac49af22472839d6a480a320c500a08061
Hajin Yun <faiith7001@gmail.com>
2024-01-09 13:32:20 +0900

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ import logoImg from '../assets/logo.jpg';
 
 export default function Header(){
 
-  // button은 cart를 위한 button
+  // button은 cart를 위한 button - handleClick function 만들기 
+  // modal에 연결하기 cart 누르면 modal이 나오도록 
   return (
     <div id="main-header">
       <div id="title">
